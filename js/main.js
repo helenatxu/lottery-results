a8acff40e072f6fd2fa1855431feb495aef85ace
Helena <helenaenred@gmail.com>
2017-05-02 11:45:57 +0700

    Google Maps deleted

--- js/main.js
+++ js/main.js
@@ -1,53 +1,39 @@
 $(document).ready(function(){
 
 
-	$("#portfolio-contant-active").mixItUp();
+    $("#portfolio-contant-active").mixItUp();
 
 
-	$("#testimonial-slider").owlCarousel({
-	    paginationSpeed : 500,      
-	    singleItem:true,
-	    autoPlay: 3000,
-	});
+    $("#testimonial-slider").owlCarousel({
+        paginationSpeed : 500,      
+        singleItem:true,
+        autoPlay: 3000,
+    });
 
 
 
 
-	$("#clients-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
+    $("#clients-logo").owlCarousel({
+        autoPlay: 3000,
+        items : 5,
+        itemsDesktop : [1199,5],
+        itemsDesktopSmall : [979,5],
+    });
 
-	$("#works-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
-
-
-	// google map
-		var map;
-		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-		    zoom: 8
-		  });
-		}
+    $("#works-logo").owlCarousel({
+        autoPlay: 3000,
+        items : 5,
+        itemsDesktop : [1199,5],
+        itemsDesktopSmall : [979,5],
+    });
 
 
-	// Counter
+    // Counter
 
-	$('.counter').counterUp({
+    $('.counter').counterUp({
         delay: 10,
         time: 1000
     });
 
 
 });
-
-
-
-
