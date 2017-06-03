113af4dc01a212e9189b034ada44894db2d65a6e
Helena <helenaenred@gmail.com>
2017-06-03 18:19:53 +0700

    Results date & locale properly formatted for moment.js

--- js/main.js
+++ js/main.js
@@ -1,48 +1,28 @@
 $(document).ready(function(){
 
-    // $("#portfolio-contant-active").mixItUp();
+    var rssUrl = "http://www.loteriasyapuestas.es/es/bonoloto/resultados/.formatoRSS";
+    function parseRSS(url, callback) {
+        $.ajax({
+            url: 'https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(url),
+            dataType: 'json',
+            success: function(data) {
+                callback(data);
+                console.log("Llamada Json completada ------",data);
+            }
+        });
+    }
+    html = "";
+    var html2 = "";
 
+    var result_final = new Array();
 
-    // $("#testimonial-slider").owlCarousel({
-    //     paginationSpeed : 500,      
-    //     singleItem:true,
-    //     autoPlay: 3000,
-    // });
+    parseRSS(rssUrl, function(rss) {
 
-
-
-
-    // $("#clients-logo").owlCarousel({
-    //     autoPlay: 3000,
-    //     items : 5,
-    //     itemsDesktop : [1199,5],
-    //     itemsDesktopSmall : [979,5],
-    // });
-
-
-        var rssUrl = "http://www.loteriasyapuestas.es/es/bonoloto/resultados/.formatoRSS";
-        function parseRSS(url, callback) {
-            $.ajax({
-                url: 'https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(url),
-                dataType: 'json',
-                success: function(data) {
-                    callback(data);
-                    console.log("json",data);
-                }
-            });
-        }
-        html = "";
-        var html2 = "";
-
-        var result_final = new Array();
-
-        parseRSS(rssUrl, function(rss) {
-
-            var results = rss.items;
-            for(i = results.length-1; i >=0; i--) {
+        var results = rss.items;
+        for(i = results.length-1; i >=0; i--) {
                 // html += "<p><a href='" + results[i].link + "'>" + results[i].title + "</a></p>";  
 
-                console.log("rss",rss);
+                // console.log("rss",rss);
 
                 var _rawResults = rss.items[i].content;
 
@@ -54,15 +34,16 @@ $(document).ready(function(){
                 result_final[i][0] = result[0].match(/\d+/g).map(function(val){
                     return val.replace(/\-/g,'');
                 });
-                console.log("result_final[i]",result_final[i]);
 
 
+                var resultsDate = new Date(rss.items[i].pubDate);
+                
 
                 result_final[i][1] = result[1].replace(/[CR]\(+\)/g,'').match(/\d+/g,'');
                 result_final[i][2] = result[2].replace(/[CR]\(+\)/g,'').match(/\d+/g,'');
-                result_final[i][3] = rss.items[i].pubDate;
+                result_final[i][3] = moment(resultsDate).format('dddd D-MMMM-YYYY');
 
-                console.log("result_final "+i, result_final[i]);
+
 
                 html2 +="<tr><td style='text-align: center;'>"+result_final[i][3]+"</td>";
                 html2 += "<td style='text-align: center; white-space: nowrap;' class='bonoloto'><ul class='balls'>";
