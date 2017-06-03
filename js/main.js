72681da846a70f5e11fb9fdeb6998c43fa314e22
Helena <helenaenred@gmail.com>
2017-06-03 21:24:23 +0700

    Filtered out duplicated results. Hidden Bote column

--- js/main.js
+++ js/main.js
@@ -11,18 +11,15 @@ $(document).ready(function(){
             }
         });
     }
-    html = "";
-    var html2 = "";
 
+    var htmlResultTable = "";
     var result_final = new Array();
+    var previousDate = "";
 
     parseRSS(rssUrl, function(rss) {
 
-        var results = rss.items;
-        for(i = results.length-1; i >=0; i--) {
-                // html += "<p><a href='" + results[i].link + "'>" + results[i].title + "</a></p>";  
-
-                // console.log("rss",rss);
+        // var results = rss.items;
+        for(i = rss.items.length-1; i >=0; i--) {
 
                 var _rawResults = rss.items[i].content;
 
@@ -44,26 +41,28 @@ $(document).ready(function(){
                 result_final[i][3] = moment(resultsDate).format('dddd D-MMMM-YYYY');
 
 
+                if (result_final[i][3] === previousDate) continue;
+                    else previousDate = result_final[i][3];
 
-                html2 +="<tr><td style='text-align: center;'>"+result_final[i][3]+"</td>";
-                html2 += "<td style='text-align: center; white-space: nowrap;' class='bonoloto'><ul class='balls'>";
+
+                htmlResultTable +="<tr><td style='text-align: center;'>"+result_final[i][3]+"</td>";
+                htmlResultTable += "<td style='text-align: center; white-space: nowrap;' class='bonoloto'><ul class='balls'>";
 
                 var dayResults = result_final[i];
 
 
                 for(ball = dayResults[0].length-1; ball >=0; ball--) {
-                    // console.log("html2", html2);
-                    html2 += "<li class='ball'>" + dayResults[0][ball] + "</li>";  
+                    htmlResultTable += "<li class='ball'>" + dayResults[0][ball] + "</li>";  
                 }
 
-                html2 += "<li class='bonus-ball bonus'>" + dayResults[1] + "</li>";
-                html2 += "<li class='reintegro bonus'>" + dayResults[2] + "</li>";
-                html2 += "</ul></td>";
-                html2 +="<td style='text-align: center;'>Bote acumulado</td></tr>";
+                htmlResultTable += "<li class='bonus-ball bonus'>" + dayResults[1] + "</li>";
+                htmlResultTable += "<li class='reintegro bonus'>" + dayResults[2] + "</li>";
+                htmlResultTable += "</ul></td>";
+                // htmlResultTable +="<td style='text-align: center;'></td></tr>";
+                htmlResultTable +="</tr>";
 
 
-                $("tbody#rss-results").html(html2);
-                // console.log("html2", html2);
+                $("tbody#rss-results").html(htmlResultTable);
 
             }
 
